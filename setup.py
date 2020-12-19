233e5fcd98594f4d55a93c48aa777e961200f4dc
Chazeon <chenxing.luo@gmail.com>
2020-12-19 16:30:19 -0500

    Add static Cij calculator

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     url='https://github.com/MineralsCloud/qha-cij/',
     packages=find_packages(),
     install_requires=[
-        "numpy >= 1.7.0",
+        "numpy >= 1.10.0",
+        "pandas",
         "scipy",
         "qha",
         "lazy_property",
@@ -19,14 +20,16 @@ setup(
     ],
     package_data={
         "cij/data/schema": "*.schema.json",
-        "cij/data/output": "writer_rules.yml"
+        "cij/data/output": "writer_rules.yml",
+        "cij/data/constraints": "*"
     },
     entry_points = {
         'console_scripts': [
             'qha-cij=cij.cli.main:main',
             'qha-cij-extract=cij.cli.extract:main',
             'qha-cij-plot=cij.cli.plot:main',
-            'qha-cij-modes=cij.cli.modes:main'
+            'qha-cij-modes=cij.cli.modes:main',
+            'qha-cij-static=cij.cli.static:main'
         ],
     }
 )
