528467a4a0692dbd0e71286ccfea320612ab36bf
Chazeon <chenxing.luo@gmail.com>
2021-01-01 14:52:47 -0500

    Add Cij filling functionalities based on their symmetries

---
+++ cij/cli/fill.py
@@ -0,0 +1,59 @@
+from io import StringIO
+import sys
+
+import click
+import pandas
+
+from pathlib import Path
+
+from cij.util.fill import fill_cij
+from cij.data import get_data_fname
+
+
+@click.command(help="Fill non-zero Cij terms based on symmetry")
+@click.argument("fname", type=click.Path(exists=True))
+@click.option("-c", "--constraints")
+@click.option("--ignore-residuals", is_flag=True)
+@click.option("--ignore-rank", is_flag=True)
+@click.option("--drop-atol", type=click.FLOAT)
+def main(**kwargs):
+
+    fname = kwargs.pop("fname")
+
+    with open(fname) as fp:
+
+        # Read header
+
+        sys.stdout.write(fp.readline())
+
+        line = fp.readline()
+        N = int(line.strip().split()[1])
+        sys.stdout.write(line)
+
+        # Read elasticity table
+
+        sio = StringIO()
+        for i in range(N + 1):
+            line = fp.readline()
+            sio.write(line)
+        sio.seek(0)
+
+        # Convert the constraint name to its full path
+
+
+        if kwargs["constraints"] is not None:
+            constraints_fname = Path("constraints") / kwargs["constraints"]
+            kwargs["constraints"] = get_data_fname(str(constraints_fname))
+
+        # Write elasticity table
+
+        elast = pandas.read_table(sio, header=0, index_col=None, sep="\s+")
+        elast = fill_cij(elast, **kwargs)
+        sys.stdout.write(elast.to_string(index=False) + "\n")
+
+        # Write the rest of the file
+
+        sys.stdout.write(fp.read())
+
+if __name__ == "__main__":
+    main()
---
+++ cij/util/fill.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+from collections import OrderedDict
+import itertools
+import re
+
+import pandas
+import sympy
+import numpy
+from sympy.parsing.sympy_parser import parse_expr
+
+
+def fill_cij(
+    elast: pandas.DataFrame,
+    constraints: str = None,
+    ignore_residuals: bool = False,
+    ignore_rank: bool = False,
+    drop_atol: float = None
+) -> pandas.DataFrame:
+
+    if constraints is None:
+        return elast
+
+    # use V column as index
+
+#     elast = elast.set_index("V")
+
+    # symbols = c11, c22, c33, ...
+
+    symbols = OrderedDict([
+        (f"c{i}{j}", sympy.symbols(f"c{i}{j}"))
+        for (i, j)
+        in itertools.product(range(1, 7), range(1, 7))
+        if i <= j
+    ])
+    nsym = len(symbols)
+
+    # known variables
+
+    a = []
+    b = []
+
+    for sym, col in elast.iteritems():
+        if not re.search(r"c(\d)(\d)", sym): continue
+        _a = numpy.zeros(nsym)
+        _a[list(symbols.keys()).index(sym)] = 1
+        a.append(_a)
+        b.append(col.to_numpy())
+
+    a = numpy.array(a)
+    b = numpy.array(b)
+
+    # constraints
+
+    eqns = []
+
+    with open(constraints) as fp:
+        for line in fp:
+            parts = [parse_expr(part) for part in line.split("=")]
+            for part in parts[1:]:
+                eqns.append(parts[0] - part)
+    
+    _a, _b = sympy.linear_eq_to_matrix(eqns, *symbols.values())
+    _a = numpy.array(_a).astype(numpy.float64)
+    _b = numpy.array(_b).astype(numpy.float64)
+    _b = numpy.broadcast_to(_b, (_b.shape[0], b.shape[1]))
+
+    a = numpy.concatenate((a, _a), axis=0)
+    b = numpy.concatenate((b, _b), axis=0)
+
+    # fitting
+
+    x, residuals, rank, s = numpy.linalg.lstsq(a, b)
+
+    # warning
+
+    if rank < nsym and not ignore_rank:
+        raise Warning(f"Rank of constraints {rank} is smaller than input {nsym}!")
+    
+    if numpy.any(residuals > 0.1) and not ignore_residuals:
+        raise Warning(f"Residuals seems to be too large! -> (" + ", ".join(
+            f"{sym}: {res:.3f}" for sym, res in zip(symbols, residuals)
+        ) + ")")
+    
+    # write back to table
+
+    for index, col in zip(symbols, x):
+        elast.loc[:, index] = col
+    
+    # drop empty columns
+
+    for index, col in list(elast.iteritems()):
+        if numpy.allclose(col.to_numpy(), 0, atol=drop_atol):
+            elast = elast.drop(index, axis=1)
+
+#     elast = elast.reset_index()
+
+    return elast
