96de8018928ea109c1911b46e8642510c1c3a6e7
Chazeon <chenxing.luo@gmail.com>
2021-04-13 01:38:22 -0400

    Add partial test for cij.util.units

---
+++ tests/test_cij_util_units.py
@@ -0,0 +1,20 @@
+import numpy
+import pytest
+
+
+from cij.util.units import (
+    _from_ang3, _to_ang3,
+    _from_gpa, _to_gpa
+)
+
+
+@pytest.mark.parametrize("x", numpy.random.rand(3, 3))
+def test_units(x):
+
+    # angstrom3 <-> bohr3
+    assert numpy.allclose(_from_ang3(x), x / 0.529177249 ** 3)
+    assert numpy.allclose(  _to_ang3(x), x * 0.529177249 ** 3)
+
+    # gpa <-> ryd / bohr3
+    assert numpy.allclose(_from_gpa(x), x / 14710.5076)
+    assert numpy.allclose(  _to_gpa(x), x * 14710.5076)
