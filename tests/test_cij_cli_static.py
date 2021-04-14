508e50c01192571c2b3ac32431ef5af7c4450c3f
Chazeon <chenxing.luo@gmail.com>
2021-04-14 14:01:15 -0400

    Update cij run-static test with 'system' option

--- tests/test_cij_cli_static.py
+++ tests/test_cij_cli_static.py
@@ -2,17 +2,17 @@ import pytest
 
 
 inputs = [
-    ("examples/diopside/input01", "examples/diopside/input02"),
-    ("examples/bridgmanite/input01", "examples/bridgmanite/elast.dat"),
-    ("examples/akimotoite/input01", "examples/akimotoite/input02"),
+    ("examples/diopside/input01", "examples/diopside/input02", "monoclinic"),
+    ("examples/bridgmanite/input01", "examples/bridgmanite/elast.dat", "orthorhombic"),
+    ("examples/akimotoite/input01", "examples/akimotoite/input02", "trigonal7"),
 ]
 
-@pytest.mark.parametrize("input01, input02", inputs)
-def test_cij_static_with_example(cli_runner, input01, input02):
+@pytest.mark.parametrize("input01, input02, system", inputs)
+def test_cij_static_with_example(cli_runner, input01, input02, system):
     from cij.cli.static import main
 
     result = cli_runner.invoke(main, [input01])
     assert result.exit_code == 0
 
-    result = cli_runner.invoke(main, [input01, input02])
+    result = cli_runner.invoke(main, [input01, input02, "-s", system])
     assert result.exit_code == 0
