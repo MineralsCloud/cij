b603b3d3528d5aa8527f963bdd4d4376efeea322
Chazeon <chenxing.luo@gmail.com>
2021-04-14 13:39:12 -0400

    Update live help for cij fill

--- cij/cli/fill.py
+++ cij/cli/fill.py
@@ -1,11 +1,11 @@
 import click
 
-@click.command("fill", help="Fill non-zero Cij terms based on symmetry.")
-@click.argument("input02", type=click.Path(exists=True))
-@click.option("-s", "--system")
-@click.option("--ignore-residuals", is_flag=True)
-@click.option("--ignore-rank", is_flag=True)
-@click.option("--drop-atol", type=click.FLOAT, default=1e-8)
+@click.command("fill", help="Fill Cij tensor components based on the symmetry constraints of the crystal system.")
+@click.argument("input02", type=click.Path(exists=True), help="The table of elastic tensor components at different volume points as input.")
+@click.option("-s", "--system", help="Name of the crystal system whose symmetry is applied to fill the missing elastic tensor components. Should be Should be one of: ``triclinic``, ``monoclinic``, ``hexagonal``, ``trigonal6``, ``trigonal8``, ``orthorhombic``, ``tetragonal6``, ``tetragonal7``, ``cubic``.")
+@click.option("--ignore-residuals", is_flag=True, help="Keep filling the missing elastic tensor components based on the crystal system even if the disagreements exceeds tolerance.")
+@click.option("--ignore-rank", is_flag=True, help="Keep filling the missing elastic tensor components based on the crystal system even if not all necessary components are give.")
+@click.option("--drop-atol", type=click.FLOAT, default=1e-8, help="Drop the elastic tensor components if the magnitude of the elastic tenor components if this components in all volume points are less than this value.")
 def main(**kwargs):
 
     import pandas
