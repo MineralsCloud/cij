47cb65bde377ca131cff94fcdd80274b5fdac67f
Chazeon <chenxing.luo@gmail.com>
2021-04-18 13:41:27 -0400

    Add the cij.misc.evec_disp2eig to cij.misc

--- cij/misc/__init__.py
+++ cij/misc/__init__.py
@@ -1,7 +1,9 @@
 from .evec_sort import evec_sort
 from .evec_load import evec_load
+from .evec_disp2eig import evec_disp2eig
 
 __all__ = [
     "evec_sort",
-    "evec_load"
+    "evec_load",
+    "evec_disp2eig"
 ]
